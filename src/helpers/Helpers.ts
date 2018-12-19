6cd2f25a3e9add4e6e91b80e03d50c2703703ec0
Elisha Witte <elishawitte@gmail.com>
2018-12-19 21:42:22 +0100

    Add helper function to parse TOML file

--- src/helpers/Helpers.ts
+++ src/helpers/Helpers.ts
@@ -1,5 +1,16 @@
 const { execSync } = require('child_process');
+const parse = require('@iarna/toml/parse-string');
 
 export const displayHelp = (command: string | undefined) => {
   execSync(`comet ${command} --help`, { stdio: [0, 1, 2] });
+};
+
+export const parseToml = (path: string, content: string) => {
+  try {
+    return parse(content);
+  } catch (error) {
+    // More readable error message
+    error.message = `TOML Error in ${path}:\n${error.message}`;
+    throw error;
+  }
 };
