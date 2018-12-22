ea7c643b7e08edd203be6cbee0971b243f713240
Elisha Witte <elishawitte@gmail.com>
2018-12-22 12:54:20 +0100

    Improve documentation

--- packages/core/src/application/BaseCommand.ts
+++ packages/core/src/application/BaseCommand.ts
@@ -6,6 +6,11 @@ export default abstract class BaseCommand extends Command {
   /** Comet config repository */
   protected configRepository: ConfigRepository;
 
+  /**
+   * BaseCommand constructor.
+   * @param argv
+   * @param config
+   */
   constructor(argv: string[], config: Config.IConfig) {
     super(argv, config);
     this.configRepository = new ConfigRepository();
