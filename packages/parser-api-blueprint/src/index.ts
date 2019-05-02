6bf73c9784569be7a3b8f59247edbc3a27e34cab
Elisha Witte <elishawitte@gmail.com>
2019-05-02 03:59:26 +0200

    Parse parameters and operations

--- packages/parser-api-blueprint/src/index.ts
+++ packages/parser-api-blueprint/src/index.ts
@@ -1,13 +1,14 @@
 import {
   ApiModel, Authentication,
   CommandConfig,
-  Information,
-  LoggerInterface,
+  Information, JsonSchema,
+  LoggerInterface, Operation, Parameter,
   ParserInterface, Resource, ResourceGroup,
 } from '@comet-cli/types';
 import * as get from 'lodash/get';
 import { readFile, writeFile } from 'fs-extra';
 import ParsingException from './ParsingException';
+import { Mson } from '../types/mson';
 const { promisify } = require('util');
 const drafter = require('drafter');
 
@@ -102,15 +103,88 @@ export default class ApiBlueprintParser implements ParserInterface {
       return item.element === 'resource';
     });
     for (const resource of astResources) {
-      resources.push({
-        path: resource.uriTemplate,
-        name: resource.name,
-        description: resource.description,
-        parameters: [],
-        operations: [],
-      });
+      const actions = [];
+      // API Blueprint lets actions define their own URIs.
+      // If the action URI is different from the resource URI (excluding query parameters)
+      // there should be a new resource for it.
+      for (const action of resource.actions || []) {
+        const uri = get(action, 'attributes.uriTemplate');
+        if (uri === undefined) continue;
+        const trimmedUri = uri.replace(/{[?&#+].+}/g, '');
+        if (trimmedUri === resource.uriTemplate) {
+          actions.push(action);
+          continue;
+        }
+        const existingResource = resources.find(item => item.path === trimmedUri);
+        if (existingResource !== undefined) {
+          existingResource.operations.push(this.parseOperation(existingResource.path, action));
+        } else {
+          const operation = this.parseOperation(resource.path, action);
+          resources.push({
+            path: trimmedUri,
+            name: null,
+            description: null,
+            parameters: this.parseParameters(resource.uriTemplate, resource.parameters),
+            operations: [operation],
+          });
+        }
+      }
+
+      if (actions.length > 0) {
+        const operations: Operation[] = [];
+        for (const action of actions) {
+          operations.push(this.parseOperation(resource.uriTemplate, action));
+        }
+        resources.push({
+          path: resource.uriTemplate,
+          name: resource.name,
+          description: resource.description,
+          parameters: this.parseParameters(resource.uriTemplate, resource.parameters),
+          // tslint:disable-next-line:object-shorthand-properties-first
+          operations,
+        });
+      }
     }
     return resources;
+  }
+
+  protected parseParameters(uri: string, params): Parameter[] {
+    const parameters: Parameter[] = [];
+    for (const param of params) {
+      const regex = new RegExp(`{[?&#+].*${param.name}`);
+      let location;
+      let defaultRequired;
+      if (regex.test(uri) === true) {
+        location = 'query';
+        defaultRequired = false;
+      } else {
+        location = 'path';
+        defaultRequired = true;
+      }
+      parameters.push({
+        location,
+        name: param.name,
+        description: param.description.trim(),
+        required: param.required || defaultRequired,
+        example: param.example || null,
+        deprecated: false,
+        schema: this.transformMsonToJsonSchema({ type: param.type, default: param.default, values: param.values }),
+      });
+    }
+    return parameters;
+  }
+
+  protected parseOperation(resourceUri: string, action: any): Operation {
+    const uri = get(action, 'attributes.uriTemplate', resourceUri);
+    return {
+      name: action.name,
+      method: action.method,
+      description: action.description || null,
+      parameters: this.parseParameters(uri, action.parameters),
+      request: null,
+      responses: [],
+      deprecated: false,
+    };
   }
 
   protected parseMetadata(result: any) {
@@ -119,5 +193,34 @@ export default class ApiBlueprintParser implements ParserInterface {
       metadata[item.name] = item.value;
     }
     return metadata;
+  }
+
+  protected transformMsonToJsonSchema(data: Mson): JsonSchema {
+    if (this.isValidType(data.type) === false) {
+      throw new ParsingException(`Invalid type definition: ${data.type}`);
+    }
+
+    const schema: JsonSchema = {
+      $schema: 'http://json-schema.org/draft-04/schema#',
+      default: data.default,
+      type: data.type,
+    };
+
+    // If it's an enum (values array present), adjust the schema
+    if (data.values && data.values.length > 0) {
+      const values = data.values ? data.values.map(item => item.value) : [];
+      schema.type = 'array';
+      schema.items = {
+        $schema: 'http://json-schema.org/draft-04/schema#',
+        type: data.type,
+        enum: values,
+      };
+    }
+
+    return schema;
+  }
+
+  protected isValidType(type: string): boolean {
+    return ['null', 'boolean', 'object', 'array', 'number', 'integer', 'string'].includes(type);
   }
 }
---
+++ packages/parser-api-blueprint/types/mson.d.ts
@@ -0,0 +1,10 @@
+export interface Mson {
+  type: string;
+  default?: string;
+  required?: boolean;
+  values?: EnumValue[];
+}
+
+export interface EnumValue {
+  value: string;
+}
